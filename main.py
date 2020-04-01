5090a95f66ef18f96a82415a3bcd731b4b2cbf6a
Adolfo Gastalver Rubio <agastalver@ingelectus.com>
2020-04-01 16:55:07 +0200

    adding lockdown parameter delta

--- main.py
+++ main.py
@@ -56,20 +56,61 @@ dft["infected"] = dft["cases"] - dft["recovered"]
 
 # optimization SIR
 
+inf0 = dft["infected"].values[0]
+rec0 = dft["recovered"].values[0]
+
 def sir(N, beta, gamma, days):
-    I0 = 1
-    R0 = 0
+    I0 = inf0
+    R0 = rec0
     S0 = N - I0 - R0
+
     def deriv(y, t, N, beta, gamma):
         S, I, R = y
         dSdt = -beta * S * I / N
         dIdt = beta * S * I / N - gamma * I
         dRdt = gamma * I
         return dSdt, dIdt, dRdt
-    y0 = S0, I0, R0
+    
     t = np.linspace(0, days, days)
+    y0 = S0, I0, R0
     ret = odeint(deriv, y0, t, args=(N, beta, gamma))
     S, I, R = ret.T
+
+    return S, I, R
+
+def sir_lockdown(N, beta, gamma, days, delta, lckday1, lckday2):
+    I0 = inf0
+    R0 = rec0
+    S0 = N - I0 - R0
+
+    def deriv(y, t, N, beta, gamma, delta):
+        S, I, R = y
+        dSdt = -beta * delta * S * I / N
+        dIdt = beta * delta * S * I / N - gamma * I
+        dRdt = gamma * I
+        return dSdt, dIdt, dRdt
+    
+    t = np.linspace(0, days, days)
+
+    y0 = S0, I0, R0
+    t0 = t[:min(lckday1, days)]
+    ret = odeint(deriv, y0, t0, args=(N, beta, gamma, 1))
+    S, I, R = ret.T
+
+    if lckday1 < days:
+        y1 = S[-1], I[-1], R[-1]
+        t1 = t[lckday1-1:min(lckday2, days)]
+        ret = odeint(deriv, y1, t1, args=(N, beta, gamma, delta))
+        S1, I1, R1 = ret.T
+        S, I, R = np.concatenate((S, S1[1:])), np.concatenate((I, I1[1:])), np.concatenate((R, R1[1:]))
+
+    if lckday2 < days:
+        y2 = S1[-1], I1[-1], R1[-1]
+        t2 = t[lckday2-1:]
+        ret = odeint(deriv, y2, t2, args=(N, beta, gamma, 1))
+        S2, I2, R2 = ret.T
+        S, I, R = np.concatenate((S, S2[1:])), np.concatenate((I, I2[1:])), np.concatenate((R, R2[1:]))
+
     return S, I, R
 
 def fdelay(delay):
@@ -78,37 +119,62 @@ def fdelay(delay):
         beta = x[1]
         gamma = x[2]
         days = len(dft)
-        S, I, R = sir(N, beta, gamma, days + abs(delay))
-        lim = abs(delay)
-        S, I, R = S[lim:lim+days], I[lim:lim+days], R[lim:lim+days]
+        S, I, R = sir(N, beta, gamma, days + delay)
+        S, I, R = S[delay:delay+days], I[delay:delay+days], R[delay:delay+days]
         Io, Ro = dft["infected"].values, dft["recovered"].values
-        So = N - Io
+        #So = N - Io
         #loss = ((S - So)**2).sum()/days + ((I - Io)**2).sum()/days + ((R - Ro)**2).sum()/days
         loss = ((I - Io)**2).sum()/days + ((R - Ro)**2).sum()/days
         return loss
-    result = optimize.minimize(f, [80000, 1, 1], method="Nelder-Mead")
+
+    result = optimize.minimize(f, [100000, 1, 1], method="Nelder-Mead")
+
     return result
 
-delay = 0
+def fdelay_lockdown(delay, lckday, nlckdays):
+    def f(x):
+        N = x[0]
+        beta = x[1]
+        gamma = x[2]
+        delta = x[3]
+        days = len(dft)
+        S, I, R = sir_lockdown(N, beta, gamma, days + delay, delta, lckday + delay, lckday + nlckdays + delay)
+        S, I, R = S[delay:delay+days], I[delay:delay+days], R[delay:delay+days]
+        Io, Ro = dft["infected"].values, dft["recovered"].values
+        #So = N - Io
+        #loss = ((S - So)**2).sum()/days + ((I - Io)**2).sum()/days + ((R - Ro)**2).sum()/days
+        loss = ((I - Io)**2).sum()/days + ((R - Ro)**2).sum()/days
+        return loss
+
+    result = optimize.minimize(f, [100000, 1, 1, 1], method="Nelder-Mead")
+
+    return result
+
+lckday = dft.index.get_loc(pd.to_datetime("2020-03-15"))
+nlckdays = 30 # days
+
+delay = 0 # days back
 result = fdelay(delay)
-for d in range(20):
-    r2 = fdelay(d)
-    print("delay: {}, fun: {}".format(d, r2.fun))
-    if r2.fun < result.fun:
+for d in range(10):
+    #res = fdelay(d)
+    res = fdelay_lockdown(d, lckday, nlckdays)
+    print("delay: {}, fun: {}".format(d, res.fun))
+    if res.fun < result.fun:
         delay = d
-        result = r2
+        result = res
 
 N = result.x[0]
 beta = result.x[1]
 gamma = result.x[2]
+delta = result.x[3]
 days = len(dft)
 
-print("optimal: N = {}, beta = {}, gamma = {}, delay = {}".format(N, beta, gamma, delay))
+print("optimal: N = {}, beta = {}, gamma = {}, delta = {}, delay = {}".format(N, beta, gamma, delta, delay))
 print("error: {}".format(result.fun))
 
-S, I, R = sir(N, beta, gamma, days + abs(delay))
-lim = abs(delay)
-S, I, R = S[lim:lim+days], I[lim:lim+days], R[lim:lim+days]
+#S, I, R = sir(N, beta, gamma, days + delay)
+S, I, R = sir_lockdown(N, beta, gamma, days + delay, delta, lckday + delay, lckday + nlckdays + delay)
+S, I, R = S[delay:delay+days], I[delay:delay+days], R[delay:delay+days]
 
 dft["S"] = S
 dft["I"] = I
@@ -120,9 +186,9 @@ dft["susceptible"] = N - dft["infected"]
 
 far = 60 # days
 
-S, I, R =  sir(N, beta, gamma, days + far + abs(delay))
-lim = abs(delay)
-S, I, R = S[lim:lim+days+far], I[lim:lim+days+far], R[lim:lim+days+far]
+#S, I, R =  sir(N, beta, gamma, days + far + delay)
+S, I, R = sir_lockdown(N, beta, gamma, days + far + delay, delta, lckday + delay, lckday + nlckdays + delay)
+S, I, R = S[delay:delay+days+far], I[delay:delay+days+far], R[delay:delay+days+far]
 d = {
     "S": S,
     "I": I,
@@ -173,8 +239,11 @@ plt.savefig(os.path.join("images", "generated-deadpct.png"), format="png", dpi=3
 plt.savefig(os.path.join("images", "generated-deadpct.pdf"), format="pdf", dpi=300, metadata=metadata)
 
 fig, ax = plt.subplots(figsize=(8,6))
-dff[["susceptible", "infected", "recovered"]].plot(ax=ax)
+dff[["susceptible"]].plot(ax=ax, alpha=0.5)
+dff[["infected", "recovered"]].plot(ax=ax)
 dff[["S", "I", "R"]].plot(ax=ax, linestyle=":")
+ax.axvspan(dff.index[lckday], dff.index[lckday+nlckdays], alpha=0.1, color='red', label="_lockdown")
+plt.text(dff.index[lckday+nlckdays-3], N*0.89, 'lockdown', color='red', alpha=0.5, rotation=90)
 ax.set_title("SIR model")
 ax.set_xlabel("")
 ax.set_ylabel("# of people")
@@ -186,6 +255,8 @@ plt.savefig(os.path.join("images", "generated-sir.pdf"), format="pdf", dpi=300, 
 fig, ax = plt.subplots(figsize=(8,6))
 dff[["cases"]].plot(ax=ax)
 dff[["forecast"]].plot(ax=ax, linestyle=":")
+ax.axvspan(dff.index[lckday], dff.index[lckday+nlckdays], alpha=0.1, color='red', label="_lockdown")
+plt.text(dff.index[lckday+nlckdays-3], N*0.85, 'lockdown', color='red', alpha=0.5, rotation=90)
 ax.set_title("Cases forecasting")
 ax.set_xlabel("")
 ax.set_ylabel("# of occurences")
